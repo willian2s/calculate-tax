7af2001f2f6c00af67d382a2b71715181dacd0e6
Willian Silva <silva.williansantos@gmail.com>
2023-03-09 09:24:50 -0300

    feat: :sparkles: Implement rabbitMQ consumer

--- cmd/consumer/main.go
+++ cmd/consumer/main.go
@@ -6,10 +6,14 @@ import (
 	"fmt"
 
 	ckafka "github.com/confluentinc/confluent-kafka-go/kafka"
-	_ "github.com/mattn/go-sqlite3"
+	amqp "github.com/rabbitmq/amqp091-go"
 	"github.com/willian2s/calculate-tax/internal/infra/database"
 	"github.com/willian2s/calculate-tax/internal/usecase"
 	"github.com/willian2s/calculate-tax/pkg/kafka"
+	"github.com/willian2s/calculate-tax/pkg/rabbitmq"
+
+	// SQLite3 Driver
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -22,16 +26,32 @@ func main() {
 
 	repository := database.NewOrderRepository(db)
 	usecase := usecase.CalculateFinalPrice{OrderRepository: repository}
-	msgChanKafka := make(chan *ckafka.Message)
 
+	// Kafka
+	msgChanKafka := make(chan *ckafka.Message)
 	topics := []string{"orders"}
 	servers := "host.docker.internal:9094"
 
+	fmt.Println("Kafka consumer has starter")
 	go kafka.Consume(topics, servers, msgChanKafka)
-	kafkaWorker(msgChanKafka, usecase)
+	go kafkaWorker(msgChanKafka, usecase)
+
+	// RabbitMQ
+	ch, err := rabbitmq.OpenChannel()
+	if err != nil {
+		panic(err)
+	}
+	defer ch.Close()
+
+	msgRabbitmqChannel := make(chan amqp.Delivery)
+
+	fmt.Println("RabbitMQ consumer has starter")
+	go rabbitmq.Consume(ch, msgRabbitmqChannel)
+	rabbitmqWorker(msgRabbitmqChannel, usecase)
 }
 
 func kafkaWorker(msgKafka chan *ckafka.Message, uc usecase.CalculateFinalPrice) {
+	fmt.Println("Kafka worker has starter")
 	for msg := range msgKafka {
 		var OrderInputDTO usecase.OrderInputDTO
 		err := json.Unmarshal(msg.Value, &OrderInputDTO)
@@ -43,6 +63,25 @@ func kafkaWorker(msgKafka chan *ckafka.Message, uc usecase.CalculateFinalPrice) 
 		if err != nil {
 			panic(err)
 		}
-		fmt.Printf("Kafka has processed order %s", outputDto.ID)
+		fmt.Printf("Kafka has processed order %s\n", outputDto.ID)
+	}
+}
+
+func rabbitmqWorker(msgChan chan amqp.Delivery, uc usecase.CalculateFinalPrice) {
+	fmt.Println("Rabbitmq worker has started")
+	for msg := range msgChan {
+		var OrderInputDTO usecase.OrderInputDTO
+		err := json.Unmarshal(msg.Body, &OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		outputDto, err := uc.Execute(OrderInputDTO)
+		if err != nil {
+			panic(err)
+		}
+
+		msg.Ack(false)
+		fmt.Printf("Rabbitmq has processed order %s\n", outputDto.ID)
 	}
 }
--- go.mod
+++ go.mod
@@ -11,5 +11,6 @@ require (
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/mattn/go-sqlite3 v1.14.16
 	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rabbitmq/amqp091-go v1.7.0
 	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
